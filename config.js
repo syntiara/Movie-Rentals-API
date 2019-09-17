2a477ceb7203b4b41b25219142383d2f5a955319
cynthia okoliezeh <cynthia.okoliezeh@gmail.com>
2019-09-17 16:38:27 +0100

    made change in config file

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ dotenv.config();
 
 module.exports = {
     port: process.env.PORT,
-    database: process.env.VIDLY_DB,
-    secretKey: process.env.JWT_PRIVATEKEY
+    database: (process.env.NODE_ENV == 'development' || process.env.NODE_ENV == 'production') ? process.env.VIDLY_DB : process.env.DB_TEST_URL,
+    secretKey: process.env.JWT_PRIVATEKEY,
 }
