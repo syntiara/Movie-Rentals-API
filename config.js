b677949890512d8cdb7ea94066b235ac158a3878
cynthia okoliezeh <cynthia.okoliezeh@gmail.com>
2019-09-19 18:21:36 +0100

    modified environment variables

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ dotenv.config();
 
 module.exports = {
     port: process.env.PORT,
-    database: (process.env.NODE_ENV == 'development' || process.env.NODE_ENV == 'production') ? process.env.MONGODB_URI : process.env.DB_TEST_URL,
+    database: (process.env.NODE_ENV == 'development' || process.env.NODE_ENV == 'production') ? process.env.MONGOLAB_GREEN_URI : process.env.DB_TEST_URL,
     secretKey: process.env.JWT_PRIVATEKEY,
 }
