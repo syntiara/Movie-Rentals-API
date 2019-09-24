15bdcdcdbfb0ba69c678185e0c29f720ddbd828a
cynthia okoliezeh <cynthia.okoliezeh@gmail.com>
2019-09-24 20:40:22 +0100

    bug fix

--- server/middleware/validation.js
+++ server/middleware/validation.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 export const validateObjectId =  (req, res, next) => {
-    return (mongoose.Types.ObjectId.isValid(req.params.id)) ? next() :  res.status(404).send('invalid Id');
+    return (mongoose.Types.ObjectId.isValid(req.params.id)) ? next() :  res.status(400).send('invalid Id');
 }
 
 export const requestValidator = (validator) => {
